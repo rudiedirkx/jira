e0ccd7fef742660d8867a649cdc7482ea336d8e7
Rudie Dirkx <github@hotblocks.nl>
2014-12-16 23:50:02 +0100

    #26 Tempo done

--- tempo.php
+++ tempo.php
@@ -8,6 +8,23 @@ $tempo = jira_get('/rest/tempo-timesheets/1/user/issues/ASS', array(
 	'username' => JIRA_USER,
 ), $error, $info);
 
+if ($error) {
+	include 'tpl.header.php';
+
+	if ($error == 404) {
+		echo '<p>No <strong>Tempo</strong> in this house...</p>';
+	}
+	else {
+		echo '<pre>';
+		var_dump($error);
+		print_r($info);
+		echo '</pre>';
+	}
+
+	include 'tpl.footer.php';
+	exit;
+}
+
 // Group by date, index by issue
 $dated = $issues = $totals = array();
 foreach ( $tempo->timesheetLines as $i => $line ) {
