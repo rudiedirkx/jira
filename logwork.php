1c34a9ce20bd7f4b3436695ea441e02fa3f6e6fe
Rudie Dirkx <github@hotblocks.nl>
2015-10-03 22:12:07 +0200

    #59 worklog input+

--- logwork.php
+++ logwork.php
@@ -10,10 +10,15 @@ $id = @$_GET['id'];
 $summary = @$_GET['summary'];
 
 // Post worklog
-if ( isset($_POST['spent'], $_POST['date'], $_POST['description']) ) {
+if ( isset($_POST['spent'], $_POST['date'], $_POST['time'], $_POST['description']) ) {
+	$utc = strtotime($_POST['date'] . ' ' . $_POST['time']);
+	if ( !$_POST['date'] || !$_POST['date'] || !$utc ) {
+		exit('Unrecognizable date/time format... Try <code>YYYY-MM-DD</code> and <code>HH:MM</code>');
+	}
+
 	$data = array(
 		'timeSpent' => $_POST['spent'],
-		'started' => date(WORKLOG_DATETIME, strtotime($_POST['date'])),
+		'started' => date(WORKLOG_DATETIME, $utc),
 		'comment' => $_POST['description'],
 	);
 
@@ -44,13 +49,24 @@ $worklog = $id ? jira_get('issue/' . $key . '/worklog/' . $id) : false;
 echo '<h1><a href="issue.php?key=' . $key . '">' . $key . '</a> ' . html($summary) . '</h1>';
 
 ?>
+<style>
+input[name="date"],
+input[name="time"] {
+	width: 47% !important;
+}
+</style>
+
 <h2><?= $worklog ? 'Edit worklog # ' . $worklog->id : 'Log work' ?></h2>
 
 <form autocomplete="off" method="post">
 	<p>Time spent: <input name="spent" placeholder="&quot;30m&quot; for 30 minutes, &quot;2h&quot; for 2 hours, etc" value="<?= html(@$worklog->timeSpent) ?>" /></p>
-	<p>Date started: <input name="date" value="<?= date('Y-m-d H:i:s', $worklog ? strtotime($worklog->started) : time()) ?>" /></p>
+	<p>
+		Date started:<br />
+		<input type="date" name="date" value="<?= date('Y-m-d', $worklog ? strtotime($worklog->started) : time()) ?>" />
+		<input type="time" name="time" value="<?= date('H:i', $worklog ? strtotime($worklog->started) : time()) ?>" />
+	</p>
 	<p>Description: <input name="description" value="<?= html(@$worklog->comment) ?>" /></p>
-	<p><input type="submit" /></p>
+	<p><button>Submit</button></p>
 </form>
 
 <?php
